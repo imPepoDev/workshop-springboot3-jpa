afb63945698c13c39d1ccc86f6c4e864b53cba83
imPepoDev <pedrogabrielofc@hotmail.com>
2025-02-17 16:55:24 -0300

    Service layer, component registration

--- src/main/java/net/pepodev/course/repositories/UserRepository.java
+++ src/main/java/net/pepodev/course/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package net.pepodev.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import net.pepodev.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {}
---
+++ src/main/java/net/pepodev/course/services/UserService.java
@@ -0,0 +1,27 @@
+package net.pepodev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.pepodev.course.entities.User;
+import net.pepodev.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository rep;
+	
+	public List<User> findAll() {
+		return rep.findAll();
+	}
+	
+	public User findById(Integer id) {
+		Optional<User> userID = rep.findById(id);
+		return userID.get();
+	}
+	
+}
