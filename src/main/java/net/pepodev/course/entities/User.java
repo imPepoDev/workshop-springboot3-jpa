1ff6195f0d7fa3c5c0d2bb15c7ec7964aadf872d
imPepoDev <pedrogabrielofc@hotmail.com>
2025-02-18 14:23:21 -0300

    Order, instant, ISO 8601

--- src/main/java/net/pepodev/course/entities/User.java
+++ src/main/java/net/pepodev/course/entities/User.java
@@ -1,12 +1,15 @@
 package net.pepodev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String pass;
+	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -74,6 +81,10 @@ public class User implements Serializable {
 
 	public void setPass(String pass) {
 		this.pass = pass;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
